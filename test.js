87e5f9cb0e20f3ed5275be6e6cf6515fd785fb52
ChrisPecc <pecceu.christophe@gmail.com>
2020-05-21 11:58:47 +0200

    commit js avant mise en forme du texte

--- test.js
+++ test.js
@@ -1,4 +1,36 @@
-var element = document.getElementsByClassName("contact")
-element.onclick = changestyleside(){
-    alert("click")
+/* A terme, j'vais essayer de faire que deux fonction en récupérant l'id lors du clic, mais pour l'instant je n'y arrive pas */
+
+function changecontact() {
+    var element = document.getElementById("contact");
+    element.classList.toggle("sidesectionbig");
+} 
+
+function changelang() {
+    var element = document.getElementById("languages");
+    element.classList.toggle("sidesectionbig");
+}
+
+function changeadv() {
+    var element = document.getElementById("advantages");
+    element.classList.toggle("sidesectionbig");
+}
+
+function changequal() {
+    var element = document.getElementById("qualities");
+    element.classList.toggle("sidesectionbig");
+}
+
+function changeform() {
+    var element = document.getElementById("formation");
+    element.classList.toggle("mainsectionbig");
+}
+
+function changepro() {
+    var element = document.getElementById("proexperience");
+    element.classList.toggle("mainsectionbig");
+}
+
+function changeother() {
+    var element = document.getElementById("otherexperience");
+    element.classList.toggle("mainsectionbig");
 }
